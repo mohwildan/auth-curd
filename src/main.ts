30a860f718986af7274820da5cb46af8b1a41c28
mohwildan <mobafreewtf@gmail.com>
2022-11-30 06:10:30 +0700

    fix(fix cors origin): cors origin app module

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
