ad1f5f842601491734019e09df68e14f709153ad
mohwildan <mobafreewtf@gmail.com>
2022-11-30 00:48:23 +0700

    feat(find my  user): add my user and all users for database

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { jwtAuhtGuard } from 'src/auth/jwt.guard';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UseGuards(jwtAuhtGuard)
+  @Get(':id')
+  getMyUser(@Param() param: { id: string }, @Req() req) {
+    return this.userService.getMyUser(param.id, req);
+  }
+
+  @Get()
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { Request } from 'express';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async getMyUser(id: string, req: Request) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: String(id),
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    const decodedUser = req.user as {
+      id: string;
+      email: string;
+      name: string;
+      userName: string;
+      imageProfile: string;
+    };
+
+    if (user.id !== decodedUser.id) {
+      throw new ForbiddenException();
+    }
+
+    return { user: req.user };
+  }
+
+  async getAllUsers() {
+    const users = await this.prisma.user.findMany();
+    return {
+      users,
+    };
+  }
+}
