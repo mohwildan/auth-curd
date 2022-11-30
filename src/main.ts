4ce7e926e5c21a2b9cf027e38caac8ce7f2e0268
mohwildan <mobafreewtf@gmail.com>
2022-11-30 10:26:31 +0700

    feat(add credetials): add credentials

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({
     origin: ['http://localhost:3000'],
+    credentials: true,
   });
   await app.listen(process.env.PORT || 3001);
 }
